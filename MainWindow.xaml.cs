fa5fcb145a59dd063ab98c04e2e8032a58c31a5d
Ivan Tomov <ivanb.tomov1@gmail.com>
2024-11-14 15:47:19 +0200

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace AsyncWpfApp
+{
+    public partial class MainWindow : Window
+    {
+        private bool isRunning = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            if (isRunning) return;
+
+            isRunning = true;
+            btnStart.IsEnabled = false;
+
+            DateTime startTime = DateTime.Now;
+            lblTime.Content = $"Start Time: {startTime:HH:mm:ss}";
+            lblResult.Content = "";
+
+            await SlowWork();
+
+            DateTime endTime = DateTime.Now;
+            lblTime.Content += $"\nEnd Time: {endTime:HH:mm:ss}";
+
+            isRunning = false;
+            btnStart.IsEnabled = true;
+        }
+
+        private async Task SlowWork()
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                int number = random.Next(1, 101);
+                lblResult.Content = $"Generated Number: {number}";
+
+                
+                await Task.Delay(random.Next(100, 201));
+            }
+        }
+    }
+}
